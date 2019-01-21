e1de603f6b8eaaeaa4da965ee847e6af300d316a
Phillip Brown <phillip@Phillips-MacBook-Pro.local>
2019-01-21 11:13:38 +1030

    optimisation approach changed objective function

--- Chaste_models/ChasteMembrane/processing/optimise_parameters.m
+++ Chaste_models/ChasteMembrane/processing/optimise_parameters.m
@@ -11,13 +11,12 @@
 close all;
 clear all;
 
-input_vars = [10, 100, 0.7]; % ees, ms, vf
-step_sizes = [5, 5, 0.05];
+input_vars = [30, 100, 0.75]; % ees, ms, vf
+step_sizes = [10, 10, 0.05];
 index = 1; % tracking the index of the variable we are optimising
 
-cct = 4;
+cct = 2;
 
-target_value = 100 * 15/ (10 + cct); % an estimate of the number of cells that would be produced in a healthy crypt
 
 % Run the first guess to get the ball rolling
 first_step = true;
@@ -26,13 +25,13 @@ direction = +1; % +1 means increase, -1 means decrease
 fprintf('Simulating initial point\n');
 obj = run_simulation(input_vars, cct);
 fprintf('Done\n');
-fprintf('Objective = %d, with params: EES = %g, MS = %g, VF = %g,\n', obj,input_vars(1),input_vars(2),input_vars(3));
+fprintf('Objective = %.5f, with params: EES = %g, MS = %g, VF = %g,\n', obj,input_vars(1),input_vars(2),input_vars(3));
 
 iterations = 0;
 
 fprintf('Starting loop\n');
 % Start the optimisation procedure
-while obj < target_value && iterations < 20
+while obj < 0.9 && iterations < 20
     % choose an axis, choose a direction, take steps until no improvement
     % if no improvement, halve the step size, try a new axis
     temp_vars = input_vars;
@@ -83,18 +82,23 @@ while obj < target_value && iterations < 20
         % axis, reset the stepping direction and reset the first step tracker
         step_sizes(index) = step_sizes(index)/2;
         if (index == 3); index = 1; else; index = index + 1; end
-        fprintf('Step was worse, moving to variable %d\n', index);
+        fprintf('Step did not improve objective function, moving to variable %d\n', index);
         first_step = true;
         direction = +1;
     end
-    fprintf('Objective = %d, with params: EES = %g, MS = %g, VF = %g,\n', obj,input_vars(1),input_vars(2),input_vars(3));
+    fprintf('Objective = %.5f, with params: EES = %g, MS = %g, VF = %g,\n', obj,input_vars(1),input_vars(2),input_vars(3));
     iterations = iterations + 1;
         
 end
 
 function out = objective(slough, anoikis, total)
     % A way to determine if the new position is better
-    out = (slough - anoikis);
+    expected_cell_count = uint8(100 * 15/ (10 + cct) + 20); % an estimate of the number of cells passing through the crypt
+    expected_difference = uint8(expected_cell_count * 0.92);
+
+    difference = (slough - anoikis);
+
+    obj = 0.5 * (difference/expected_difference) + 0.5 * (total/expected_cell_count);
 end
 
 function [slough, anoikis, total] = get_data(cmdout)
@@ -139,6 +143,12 @@ function obj_val = run_simulation(vars, cct)
         [slough, anoikis, total] = get_data(cmdout);
     end
 
-    obj_val = objective(slough, anoikis, total);
+    expected_cell_count = (100 * 15/ (10 + cct) + 20); % an estimate of the number of cells passing through the crypt
+    expected_difference = (expected_cell_count * 0.92);
+    
+    difference = (slough - anoikis);
+
+    obj_val = 0.5 * (difference/expected_difference) + 0.5 * (total/expected_cell_count);
+    % obj_val = objective(slough, anoikis, total);
 
 end
