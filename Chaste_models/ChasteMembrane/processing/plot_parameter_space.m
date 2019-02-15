f9a044a52358e3cb04bd8b75b9263f2ab4d90c69
luckyphill <phillip.j.brown@adelaide.edu.au>
2019-02-15 20:35:54 +1030

    minor change

--- Chaste_models/ChasteMembrane/processing/plot_parameter_space.m
+++ Chaste_models/ChasteMembrane/processing/plot_parameter_space.m
@@ -2,10 +2,15 @@
 % and produces various 2D plots of the 4D parameter space, colouring
 % the plot by the objective function
 
-n = 20:2:36;
-ees = 25:25:250;
-ms = 50:50:800;
-vf = 0.6:0.05:0.95;
+% n = 20:2:36;
+% ees = 25:25:250;
+% ms = 50:50:800;
+% vf = 0.6:0.05:0.95;
+
+n = 24:28;
+ees = 25:5:100;
+ms = 150:5:250;
+vf = 0.68:0.01:0.82;
 
 N = length(n);
 E = length(ees);
@@ -15,10 +20,12 @@ V = length(vf);
 
 cct = 5;
 
-file_path = '/Users/phillip/Research/Crypt/Data/Chaste/ParameterSearch/';
+file_path = '/Users/phillipbrown/Research/Crypt/Data/Chaste/ParameterSearch/';
 
 pspace = nan(E,M,N,V);
 
+[total_count, slough, anoikis, prolif, differ, total_end] = get_data('/Users/phillipbrown/Research/Crypt/Data/Chaste/ParameterSearch/parameter_statistics_n_26_EES_50_MS_200_VF_75_CCT_5.txt');
+objective_function(total_count, slough, anoikis, prolif, differ, total_end)
 
 for k = 1:E
 	for l = 1:M
@@ -28,6 +35,9 @@ for k = 1:E
 					file_name = sprintf('%sparameter_statistics_n_%d_EES_%d_MS_%d_VF_%g_CCT_%d.txt',file_path, n(j), ees(k), ms(l), 100 * vf(m), cct);
 					[total_count, slough, anoikis, prolif, differ, total_end] = get_data(file_name);
 					pspace(k,l,j,m) = objective_function(total_count, slough, anoikis, prolif, differ, total_end);
+                    if strcmp(file_name,'/Users/phillipbrown/Research/Crypt/Data/Chaste/ParameterSearch/parameter_statistics_n_26_EES_50_MS_200_VF_75_CCT_5.txt')
+                        fprintf('%d,%d,%d,%d\n',k,l,j,m);
+                    end
 				end
 			end
 		end
@@ -39,10 +49,10 @@ for j = 1:N
 		% Plot each slice of parameter space
 		h = figure;
 		imagesc(flipud(pspace(:,:,j,m)),'AlphaData',~isnan(flipud(pspace(:,:,j,m))), [0,50]);
-		set(gca, 'YTick', 1:10);
-		set(gca, 'YTickLabel', fliplr(25:25:250));
-		set(gca, 'XTick', 2:2:16);
-		set(gca, 'XTickLabel', 100:100:800);
+		set(gca, 'YTick', 2:2:E);
+		set(gca, 'YTickLabel', floor(fliplr(linspace(ees(1), ees(end), 8))));
+		set(gca, 'XTick', 4:4:20);
+		set(gca, 'XTickLabel', 150:25:250);
 		colorbar
 		heading = {'Objective function value for', sprintf('$n=$%d, $vf=$%g and G1 phase length = 5',n(j), vf(m))};
 		title(heading, 'Interpreter', 'latex');
@@ -53,7 +63,7 @@ for j = 1:N
 	    pos = get(h,'Position');
 	    set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
 	    
-	    print(['/Users/phillip/Research/Crypt/Images/ObjectiveFunction/MouseColonDesc/ObjectiveFunction_N_' num2str(n(j)) '_VF_' num2str(100 * vf(m)), '_CCT_' num2str(cct)],'-dpdf');
+	    print(['/Users/phillipbrown/Research/Crypt/Images/ObjectiveFunction/MouseColonDesc/ObjectiveFunction_N_' num2str(n(j)) '_VF_' num2str(100 * vf(m)), '_CCT_' num2str(cct)],'-dpdf');
 
 		close(h);
 	end
@@ -109,5 +119,8 @@ function pen = penalty(value, min, max, ramp)
         pen = abs(min - value) ^ ramp;
     end
     
+    if pen < 0
+        fprintf('Fucked up\n')
+    end
     
 end
