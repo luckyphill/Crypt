9e26b30a79b5a74b11e8dd26f56f49e7ed3db958
luckyphill <phillip.j.brown@adelaide.edu.au>
2019-04-18 18:00:16 +0930

    get the module load commands in the right order

--- Chaste_models/ChasteMembrane/phoenix/ParameterOptimistation/runopt.sh
+++ Chaste_models/ChasteMembrane/phoenix/ParameterOptimistation/runopt.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 script=$1
 
+module load matlab/2016b
+
 module load CMake
 module load netCDF-Fortran
 module unload OpenMPI
 module unload HDF5
-module load matlab/2016b
+
 matlab -nodisplay -nodesktop -r "${script}; quit()"
