f0893f83bc7cfe18784d81e0223e9e96d3f72beb
luckyphill <phillip.j.brown@adelaide.edu.au>
2019-04-18 17:48:11 +0930

    minor edit for phoenix

--- Chaste_models/ChasteMembrane/phoenix/ParameterOptimistation/drop_opt.sh
+++ Chaste_models/ChasteMembrane/phoenix/ParameterOptimistation/drop_opt.sh
@@ -1,10 +1,10 @@
 
-sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au runopt.sh Optimise_HumanColon
-sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au runopt.sh Optimise_MouseColonAsc
-sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au runopt.sh Optimise_MouseColonDesc
-sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au runopt.sh Optimise_MouseColonTrans
-sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au runopt.sh Optimise_MouseColonCaecum
-sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au runopt.sh Optimise_RatColonAsc
-sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au runopt.sh Optimise_RatColonDesc
-sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au runopt.sh Optimise_RatColonTrans
-sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au runopt.sh Optimise_RatColonCaecum
+sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au ../../phoenix/ParameterOptimistation/runopt.sh Optimise_HumanColon
+sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au ../../phoenix/ParameterOptimistation/runopt.sh Optimise_MouseColonAsc
+sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au ../../phoenix/ParameterOptimistation/runopt.sh Optimise_MouseColonDesc
+sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au ../../phoenix/ParameterOptimistation/runopt.sh Optimise_MouseColonTrans
+sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au ../../phoenix/ParameterOptimistation/runopt.sh Optimise_MouseColonCaecum
+sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au ../../phoenix/ParameterOptimistation/runopt.sh Optimise_RatColonAsc
+sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au ../../phoenix/ParameterOptimistation/runopt.sh Optimise_RatColonDesc
+sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au ../../phoenix/ParameterOptimistation/runopt.sh Optimise_RatColonTrans
+sbatch -p batch -N 1 -n 1 --time=72:00:00 --mem=2GB --mail-type=ALL --mail-user=phillip.j.brown@adelaide.edu.au ../../phoenix/ParameterOptimistation/runopt.sh Optimise_RatColonCaecum
--- Chaste_models/ChasteMembrane/phoenix/ParameterOptimistation/runopt.sh
+++ Chaste_models/ChasteMembrane/phoenix/ParameterOptimistation/runopt.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 script=$1
-VF=$2
 
 module load matlab/2016b
 matlab -nodisplay -nodesktop -r "${script}; quit()"
