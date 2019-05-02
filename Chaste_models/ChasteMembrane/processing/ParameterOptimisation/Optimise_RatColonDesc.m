57f5f843668436ca6b38e6d864db7f794174cb26
luckyphill <phillip.j.brown@adelaide.edu.au>
2019-05-02 15:13:56 +0930

    minor changes to parameter ranges

--- Chaste_models/ChasteMembrane/processing/ParameterOptimisation/Optimise_RatColonDesc.m
+++ Chaste_models/ChasteMembrane/processing/ParameterOptimisation/Optimise_RatColonDesc.m
@@ -11,7 +11,7 @@ fprintf('Optimising Rat Descending Colon parameters\n');
 
 p.input_flags= {'n','np','ees','ms','vf','cct','wt'};
 p.prange = {[36, 42], [20, 28], [50, 100], [150, 200], [0.7], [58], [12]};
-p.limits = {[30, 48], [16, 36], [10, 200], [50,  400], [0.6, 0.95], [52, 64], [8, 16]};
+p.limits = {[32, 48], [16, 36], [10, 150], [50,  400], [0.6, 0.95], [52, 64], [8, 16]};
 p.min_step_size = [1,1,1,1,0.01,1,0.5,0.5];
 
 p.static_flags = {'t'};
