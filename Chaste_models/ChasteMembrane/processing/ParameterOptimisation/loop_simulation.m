8b8e3deb01c142a47b9f19f04d8d6014ef4292c1
luckyphill <phillip.j.brown@adelaide.edu.au>
2019-05-13 17:18:17 +0930

    minor edit for phoenix

--- Chaste_models/ChasteMembrane/processing/ParameterOptimisation/loop_simulation.m
+++ Chaste_models/ChasteMembrane/processing/ParameterOptimisation/loop_simulation.m
@@ -4,6 +4,7 @@ function loop_simulation(obj, time, n, np, ees, ms, vf, cct, wt)
 	obj = str2func(obj);
 	for e = (ees-35):5:(ees+35)
 		for m = (ms-35):5:(ms+35)
+			fprintf('\n Running simulation for %s with parameters n=%g, np=%g, ees=%g, ms=%g, vf=%g, cct=%g, wt=%g\n',func2str(obj), n, np, e, m, vf, cct, wt)
 			simulate(obj, time, n, np, ees, ms, vf, cct, wt);
 		end
 	end
