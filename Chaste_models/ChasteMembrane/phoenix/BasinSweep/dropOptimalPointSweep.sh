e8f0dc1edad5faabb234dbd6051579cd47548d17
luckyphill <phillip.j.brown@adelaide.edu.au>
2020-02-25 11:13:43 +1030

    check the optimal points again

--- Chaste_models/ChasteMembrane/phoenix/BasinSweep/dropOptimalPointSweep.sh
+++ Chaste_models/ChasteMembrane/phoenix/BasinSweep/dropOptimalPointSweep.sh
@@ -1,13 +1,13 @@
 # Drop all the remaining sweeps - may as well, there's heaps of computing time left
 
-sbatch genericOptimalPointSweep.sh 1 MouseColonDesc.txt
-sbatch genericOptimalPointSweep.sh 2 MouseColonAsc.txt
-sbatch genericOptimalPointSweep.sh 3 MouseColonTrans.txt
-sbatch genericOptimalPointSweep.sh 4 MouseColonCaecum.txt
+sbatch genericOptimalPointSweep.sh 1 ../../optimal/MouseColonDesc.txt
+sbatch genericOptimalPointSweep.sh 2 ../../optimal/MouseColonAsc.txt
+sbatch genericOptimalPointSweep.sh 3 ../../optimal/MouseColonTrans.txt
+sbatch genericOptimalPointSweep.sh 4 ../../optimal/MouseColonCaecum.txt
 
-sbatch genericOptimalPointSweep.sh 5 RatColonDesc.txt
-sbatch genericOptimalPointSweep.sh 6 RatColonAsc.txt
-sbatch genericOptimalPointSweep.sh 7 RatColonTrans.txt
-sbatch genericOptimalPointSweep.sh 8 RatColonCaecum.txt
+sbatch genericOptimalPointSweep.sh 5 ../../optimal/RatColonDesc.txt
+sbatch genericOptimalPointSweep.sh 6 ../../optimal/RatColonAsc.txt
+sbatch genericOptimalPointSweep.sh 7 ../../optimal/RatColonTrans.txt
+sbatch genericOptimalPointSweep.sh 8 ../../optimal/RatColonCaecum.txt
 
 
