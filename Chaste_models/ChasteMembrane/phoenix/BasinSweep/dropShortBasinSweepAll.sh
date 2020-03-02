1dc298af206c78b6c4a921fd2397cbf2c260d00b
Phillip Brown <pbrown.mwerhun@gmail.com>
2020-03-02 14:38:55 +1030

    dropping all short sweeps

---
+++ Chaste_models/ChasteMembrane/phoenix/BasinSweep/dropShortBasinSweepAll.sh
@@ -0,0 +1,143 @@
+sbatch genericShortBasinSweep.sh 1 1 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 2 1 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 1 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 1 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 1 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 1 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 7 1 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 1 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 1 2 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 2 2 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 2 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 2 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 2 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 2 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 7 2 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 2 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 1 3 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 2 3 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 3 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 3 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 3 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 3 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 7 3 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 3 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 1 4 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 2 4 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 4 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 4 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 4 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 4 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 7 4 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 4 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 1 5 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 2 5 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 5 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 5 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 5 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 5 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 7 5 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 5 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 1 6 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 2 6 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 6 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 6 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 6 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 6 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 7 6 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 6 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 7 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 7 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 7 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 7 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 7 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 7 7 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 7 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 8 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 8 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 8 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 8 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 8 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 7 8 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 8 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 9 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 9 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 9 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 9 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 9 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 7 9 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 9 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 10 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 10 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 10 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 10 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 10 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 10 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 11 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 11 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 11 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 11 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 11 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 11 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 12 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 12 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 4 12 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 12 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 12 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 12 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 13 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 13 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 5 13 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 13 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 13 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 14 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 14 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 14 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 14 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 15 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 15 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 15 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 15 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 16 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 16 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 16 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 16 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 17 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 17 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 17 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 8 17 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 2 18 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 3 18 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 18 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 3 19 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 19 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 3 20 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 20 modifiersShort.txt
+
+sbatch genericShortBasinSweep.sh 6 21 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 22 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 23 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 24 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 25 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 26 modifiersShort.txt
+sbatch genericShortBasinSweep.sh 6 27 modifiersShort.txt
+
