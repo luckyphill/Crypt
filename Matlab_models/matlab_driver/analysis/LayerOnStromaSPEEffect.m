64c80aa8dc0280c0e5440f79e3e4fce7ceaf2da8
luckyphill <phillip.j.brown@adelaide.edu.au>
2020-08-16 17:53:31 +0930

    images

--- Matlab_models/matlab_driver/analysis/LayerOnStromaSPEEffect.m
+++ Matlab_models/matlab_driver/analysis/LayerOnStromaSPEEffect.m
@@ -112,6 +112,7 @@ classdef LayerOnStromaSPEEffect < Analysis
 					% try
 						a = RunLayerOnStroma(n,p,g,w,b,sae,spe,j);
 						a.LoadSimulationData();
+						bottom = a.data.bottomWiggleData;
 						if max(bottom) > 1.05
 							count = count + 1;
 						end
@@ -119,6 +120,8 @@ classdef LayerOnStromaSPEEffect < Analysis
 				end
 
 				result(i) = count / obj.simulationRuns;
+
+				fprintf("Completed %.2f %%\n", 100*i/length(obj.parameterSet));
 
 
 			end
@@ -132,34 +135,116 @@ classdef LayerOnStromaSPEEffect < Analysis
 
 		function PlotData(obj)
 
-			AssembleData(obj);
+			%AssembleData(obj);
 
-
-			for p = obj.p
+            % g held constant on one plot
+			for g = obj.g
 
 				h = figure;
-				for g = obj.g
+                leg = {};
+				for p = obj.p
+
+					if p~=5 || g~=5
 
 
 					
 
-					Lidx = obj.parameterSet(:,2) == p;
-					tempR = obj.result(Lidx);
-					Lidx = obj.parameterSet(Lidx,3) == g;
-					data = tempR(Lidx);
+						Lidx = obj.parameterSet(:,2) == p;
+						tempR = obj.result(Lidx);
+						Lidx = obj.parameterSet(Lidx,3) == g;
+						data = tempR(Lidx);
 
-					plot(obj.spe, data,'LineWidth', 4)
-					hold on
+						plot(obj.spe, data,'LineWidth', 4)
+						hold on
+                        
+                        leg{end+1} = sprintf('p=%g',p);
+
+					end
 					
 
 				end
 
-				ylabel('Chance of buckling','Interpreter', 'latex', 'FontSize', 15);xlabel('Perimeter force parameter','Interpreter', 'latex', 'FontSize', 15);
-				title(sprintf('Chance of buckling with p = %g', p),'Interpreter', 'latex', 'FontSize', 22);
+				ylabel('Proportion','Interpreter', 'latex', 'FontSize', 15);xlabel('Perimeter energy parameter','Interpreter', 'latex', 'FontSize', 15);
+				title(sprintf('Proportion buckled with g = %g', g),'Interpreter', 'latex', 'FontSize', 22);
 				xlim([0 10]);ylim([0 1]);
+                legend(leg);
+				SavePlot(obj, h, sprintf('PerimeterEnergy_g%g',g));
+            end
+            
+            
+            % p held constant on one plot
+            for p = obj.p
 
-				SavePlot(obj, h, sprintf('BodyParams_vs_PhaseLength'));
-			end
+				h = figure;
+                leg = {};
+				for g = obj.g
+
+					if p~=5 || g~=5
+
+
+					
+
+						Lidx = obj.parameterSet(:,2) == p;
+						tempR = obj.result(Lidx);
+						Lidx = obj.parameterSet(Lidx,3) == g;
+						data = tempR(Lidx);
+
+						plot(obj.spe, data,'LineWidth', 4)
+						hold on
+                        
+                        leg{end+1} = sprintf('g=%g',g);
+
+					end
+					
+
+				end
+
+				ylabel('Proportion','Interpreter', 'latex', 'FontSize', 15);xlabel('Perimeter energy parameter','Interpreter', 'latex', 'FontSize', 15);
+				title(sprintf('Proportion buckled with p = %g', p),'Interpreter', 'latex', 'FontSize', 22);
+				xlim([0 10]);ylim([0 1]);
+                legend(leg);
+				SavePlot(obj, h, sprintf('PerimeterEnergy_p%g',p));
+            end
+            
+            
+            % Constant cell cycle length
+            h = figure;
+            leg = {};
+            cct = 20;
+            for g = obj.g
+
+				
+				for p = obj.p
+
+					if p + g == cct
+
+
+					
+
+						Lidx = obj.parameterSet(:,2) == p;
+						tempR = obj.result(Lidx);
+						Lidx = obj.parameterSet(Lidx,3) == g;
+						data = tempR(Lidx);
+
+						plot(obj.spe, data,'LineWidth', 4)
+                        %scatter(obj.spe, data,'filled');
+						hold on
+                        
+                        leg{end+1} = sprintf('p=%g,g=%g',p,g);
+
+					end
+					
+
+				end
+
+            end
+            
+            ylabel('Proportion','Interpreter', 'latex', 'FontSize', 15);xlabel('Perimeter energy parameter','Interpreter', 'latex', 'FontSize', 15);
+            title(sprintf('Proportion buckled with p+g = %g', cct),'Interpreter', 'latex', 'FontSize', 22);
+            xlim([0 10]);ylim([0 1]);
+            legend(leg);
+            SavePlot(obj, h, sprintf('PerimeterEnergy_pg%g',cct));
+            
 
 		end
 
