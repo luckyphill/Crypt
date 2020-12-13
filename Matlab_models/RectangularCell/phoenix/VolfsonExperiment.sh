f3db5890ad38229b649f884a52bf66d7a0b0a997
Phillip Brown <pbrown.mwerhun@gmail.com>
2020-12-13 16:42:23 +1030

    New sims ready to run

---
+++ Matlab_models/RectangularCell/phoenix/VolfsonExperiment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#SBATCH -p batch 
+#SBATCH -N 1 
+#SBATCH -n 1 
+#SBATCH --time=72:00:00
+#SBATCH --mem=8GB 
+#SBATCH --array=0-10
+# NOTIFICATIONS
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=phillip.j.brown@adelaide.edu.au
+
+module load arch/haswell
+module load matlab/2019a
+
+matlab -nodisplay -nodesktop -r "cd ../../; addpath(genpath(pwd)); r = VolfsonExperiment(100, 5, 5, 40, 10, 0.1, 30, $SLURM_ARRAY_TASK_ID);r.RunToTime(200); quit()"
