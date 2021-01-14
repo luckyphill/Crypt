3d1f2d87fbf1848c78bf052c3070266ff35ed4a5
Phillip Brown <pbrown.mwerhun@gmail.com>
2021-01-14 16:20:21 +1030

    a single instance of RingBuckling run to t=200

---
+++ Matlab_models/RectangularCell/phoenix/RingBuckling.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#SBATCH -p batch 
+#SBATCH -N 1 
+#SBATCH -n 1 
+#SBATCH --time=72:00:00
+#SBATCH --mem=1GB 
+# NOTIFICATIONS
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=phillip.j.brown@adelaide.edu.au
+
+module load arch/haswell
+module load matlab
+export EDGEDIR='/hpcfs/users/a1738927/Research/Crypt/Matlab_models/RectangularCell'
+
+matlab -nodisplay -nodesktop -r "cd ../../; addpath(genpath(pwd)); t = RingBuckling(10,10,20,1);t.RunToTime(200); quit()"
